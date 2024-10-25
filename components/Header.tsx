777300ecb88143824b6725bbb7e98e022c1c9bf8
Daniel Budi Prasetyo <vyverdrake007@gmail.com>
2024-10-25 11:37:16 +0700

    fix: header color

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
 
   return (
     <>
-      <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      <nav className="bg-background fixed w-full z-20 top-0 start-0">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
             <Link href={"/"}><Image
@@ -45,12 +45,12 @@ export default function Header() {
             </svg>
           </button>
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0">
               {
                 navItems.map((item, index) => {
                   return (
                     <li key={index}>
-                      <Link href={item.url} className={`block py-2 px-3 text-white bg-transparent md:p-0 ${item.url === pathname ? 'text-primary' : ''}`}>
+                      <Link href={item.url} className={`block py-2 px-3 text-white bg-transparent md:p-0 hover:opacity-60 ${item.url === pathname ? 'text-primary' : ''}`}>
                         <span className='text-primary'>\/</span> {item.name}
                       </Link>
                     </li>
