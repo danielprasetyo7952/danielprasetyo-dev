cd2fac6d190b1f45a783a5f2d0ced810a98f5025
Daniel Budi Prasetyo <vyverdrake007@gmail.com>
2024-10-25 11:20:25 +0700

    feat: discord social and props

--- components/SocialProfiles.tsx
+++ components/SocialProfiles.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 
+type Props = {
+  type?: "horizontal" | "vertical"
+  iconSize?: number,
+  exclude?: string[]
+}
+
 const socialProfiles = [
   {
     name: "GitHub",
@@ -16,21 +22,39 @@ const socialProfiles = [
     name: "Email",
     url: "mailto:danielbudip789077@gmail.com",
     icon: "/icons/email.svg",
+  },
+  {
+    name: "Discord",
+    url: "",
+    icon: "/icons/discord.svg",
   }
 ]
 
-export default function SocialProfiles() {
+export default function SocialProfiles({
+  type = "horizontal",
+  iconSize = 30,
+  exclude = []
+} : Props) {
+  const className = type === "horizontal" ? "flex gap-4" : "flex flex-col gap-4"
+
+  let contacts: { name: string, url: string, icon: string }[] = []
+  if (exclude.length > 0) {
+    contacts = socialProfiles.filter(profile => !exclude.includes(profile.name))
+  } else {
+    contacts = socialProfiles
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center gap-4">
+    <div className={`${className} justify-center items-center`}>
       {
-        socialProfiles.map((profile, index) => (
+        contacts.map((profile, index) => (
           <Link key={index} target="_blank" href={profile.url}>
             <Image
               src={profile.icon}
-              width={30}
-              height={30}
+              width={iconSize}
+              height={iconSize}
               alt={profile.name}
-              className="cursor-pointer" />
+              className="cursor-pointer hover:opacity-50" />
           </Link>
         ))
       }
