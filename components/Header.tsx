36efe96a5c3433702e02b0892c2513fce38b53af
Daniel Budi Prasetyo <vyverdrake007@gmail.com>
2024-10-29 11:34:10 +0700

    fix: offcanvas layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -61,31 +61,33 @@ export default function Header() {
         </div>
       </nav>
 
-      <div id="drawer-navigation" className="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-y-full w-full bg-gray-800" aria-labelledby="drawer-navigation-label">
-        <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase">Menu</h5>
-        <button onClick={handleDrawer} type="button" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center hover:bg-gray-600 hover:text-white" >
-          <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-          </svg>
-          <span className="sr-only">Close menu</span>
-        </button>
-        <div className="py-4 overflow-y-auto">
-          <ul className="space-y-2 font-medium text-2xl">
-            {
-              navItems.map((item, index) => {
-                return (
-                  <li key={index}>
-                    <Link href={item.url} className={`flex items-center p-2 hover:bg-gray-700 group ${item.url === pathname ? 'text-primary' : 'text-white'}`}>
-                      <span className="text-primary pr-4">\/</span>{item.name}
-                    </Link>
-                  </li>
-                )
-              })
-            }
-          </ul>
+      <div id="drawer-navigation" className="fixed top-0 left-0 right-0 z-40 h-screen w-full p-4 transition-transform -translate-y-full bg-gray-800 flex flex-col justify-between" aria-labelledby="drawer-navigation-label">
+        <div>
+          <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase">Menu</h5>
+          <button onClick={handleDrawer} type="button" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center hover:bg-gray-600 hover:text-white" >
+            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+            <span className="sr-only">Close menu</span>
+          </button>
+          <div className="py-4 overflow-y-auto">
+            <ul className="space-y-2 font-medium text-2xl">
+              {
+                navItems.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <Link href={item.url} className={`flex items-center p-2 hover:bg-gray-700 group ${item.url === pathname ? 'text-primary' : 'text-white'}`}>
+                        <span className="text-primary pr-4">\/</span>{item.name}
+                      </Link>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
         </div>
-        <div className="absolute bottom-4 w-full">
-          <SocialProfiles exclude={["GitHub"]}/>
+        <div>
+          <SocialProfiles exclude={["GitHub"]} />
         </div>
       </div>
     </>
