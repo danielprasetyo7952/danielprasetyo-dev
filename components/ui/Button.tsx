e0835f82cdb81c0bed3c5c4554ec02c6607e29bf
Daniel Budi Prasetyo <vyverdrake007@gmail.com>
2024-10-31 16:46:12 +0700

    fix: component style

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -13,7 +13,7 @@ export default function Button({
         <div>
             <button onClick={onClick} type={type} className="relative inline-flex h-12 overflow-hidden p-[1px]">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#F9F7F7_0%,#3F72AF_50%,#F9F7F7_100%)]"></span>
-                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center bg-background py-2 px-4 text-base font-medium  backdrop-blur-3xl hover:text-[#ABB2BF]">
+                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center bg-background py-2 px-4 text-base font-medium backdrop-blur-3xl hover:text-[#ABB2BF]">
                     {children}
                 </span>
             </button>
--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -20,38 +20,31 @@ export default function Card({
     liveLink = ""
 }: Props) {
     return (
-        <div className="max-w-md border border-gray-200 rounded-lg shadow">
+        <div className="flex flex-col max-w-md border border-gray-200 rounded-lg shadow hover:transform hover:scale-105 hover:transition-transform">
             <Image
                 src={image}
                 alt=""
                 width={400}
                 height={300}
-                className="rounded-t-lg" />
-            <div className="p-5">
+                className="w-auto rounded-t-lg" />
+            <div className="p-5 flex-grow">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight">{title}</h5>
                 <p className="mb-3 font-normal text-gray-400">{description}</p>
-                <div className="flex flex-row gap-4">
-                    <Button>
-                        <Image
-                            src={"/icons/github.svg"}
-                            alt="GitHub"
-                            width={20}
-                            height={20}
-                            className="me-2" />
-                        <Link href={repoLink} className="text-sm">
-                            Repo
-                        </Link>
-                    </Button>
-                    {liveLink && (
-                        <Button>
-                            <Link href={liveLink} className="text-sm">
-                                &#128279; Live
-                            </Link>
-                        </Button>
-                    )}
-                </div>
             </div>
-        </div >
-
+            <div className="p-5 flex flex-row gap-4">
+                <Link href={repoLink} target="_blank" className="text-sm flex items-center">
+                    <Button>
+                        Repo -&gt;
+                    </Button>
+                </Link>
+                {liveLink && (
+                    <Link href={liveLink} target="_blank" className="text-sm flex items-center">
+                        <Button>
+                            Live -&gt;
+                        </Button>
+                    </Link>
+                )}
+            </div>
+        </div>
     )
 }
