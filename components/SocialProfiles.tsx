d12f9d5b5b5d9ef8fb0668a3aaa663f3e2c0491b
Daniel Budi Prasetyo <vyverdrake007@gmail.com>
2024-11-01 10:41:01 +0700

    feat: new prop username

--- components/SocialProfiles.tsx
+++ components/SocialProfiles.tsx
@@ -5,27 +5,32 @@ type Props = {
   type?: "horizontal" | "vertical"
   iconSize?: number
   exclude?: string[]
+  username?: boolean
 }
 
 const socialProfiles = [
   {
     name: "GitHub",
     url: "https://github.com/danielprasetyo7952",
+    username: "danielprasetyo7952",
     icon: "/icons/github.svg",
   },
   {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/dprasetyo7952",
+    username: "@dprasetyo7952",
     icon: "/icons/linkedin.svg",
   },
   {
     name: "Email",
     url: "mailto:danielbudip789077@gmail.com",
+    username: "danielbudip789077@gmail.com",
     icon: "/icons/email.svg",
   },
   {
     name: "Discord",
     url: "https://discordapp.com/users/690896840832450561",
+    username: "vyver7952",
     icon: "/icons/discord.svg",
   }
 ]
@@ -33,11 +38,12 @@ const socialProfiles = [
 export default function SocialProfiles({
   type = "horizontal",
   iconSize = 30,
-  exclude = []
-} : Props) {
+  exclude = [],
+  username = false
+}: Props) {
   const className = type === "horizontal" ? "flex gap-4" : "flex flex-col gap-4"
 
-  let contacts: { name: string, url: string, icon: string }[] = []
+  let contacts: { name: string, url: string, username: string, icon: string }[] = []
   if (exclude.length > 0) {
     contacts = socialProfiles.filter(profile => !exclude.includes(profile.name))
   } else {
@@ -45,16 +51,17 @@ export default function SocialProfiles({
   }
 
   return (
-    <div className={`${className} justify-center items-center`}>
+    <div className={`${className} justify-center`}>
       {
         contacts.map((profile, index) => (
-          <Link key={index} target="_blank" href={profile.url}>
+          <Link key={index} target="_blank" href={profile.url} className="flex flex-row gap-4 items-center hover:opacity-50">
             <Image
               src={profile.icon}
               width={iconSize}
               height={iconSize}
               alt={profile.name}
-              className="cursor-pointer hover:opacity-50" />
+              className="cursor-pointer" />
+            {username && <span className="text-sm">{profile.username}</span>}
           </Link>
         ))
       }
