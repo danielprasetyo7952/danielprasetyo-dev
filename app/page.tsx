a017f977fe3df3a4df34068771f0a68a062a5558
Daniel Budi Prasetyo <vyverdrake007@gmail.com>
2024-11-01 11:57:59 +0700

    feat: button disabled prop

--- app/page.tsx
+++ app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
       {/* Contact */}
       <section>
         <SectionHeader title="Contact" />
-        <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="flex flex-col gap-4">
             <p>
               I&apos;m open to any inquiries, feedback, collaboration opportunities, or freelance work. Feel free to reach out if you&apos;d like to connect and explore new possibilities in web development and technology.
@@ -110,7 +110,7 @@ export default function Home() {
               I value your feedback as it helps me improve and provide better solutions. If you need a dedicated and skilled full-stack developer or machine learning developer for your team or project, feel free to contact me. Let&apos;s collaborate to create innovative and impactful digital experiences.
             </p>
           </div>
-          <div className="border p-5 m-auto me-0">
+          <div className="border p-5 m-auto md:me-0">
             <p className="mb-3 text-lg font-semibold">Message me here</p>
             <SocialProfiles type="vertical" exclude={["GitHub"]} username={true} />
           </div>
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,17 +1,19 @@
 type Props = {
     onClick?: () => void
     type?: "button" | "submit"
+    disabled?: boolean
     children?: React.ReactNode
 }
 
 export default function Button({
     onClick,
     type = "button",
+    disabled = false,
     children
 }: Props) {
     return (
         <div>
-            <button onClick={onClick} type={type} className="relative inline-flex h-12 overflow-hidden p-[1px]">
+            <button onClick={onClick} type={type} className="relative inline-flex h-12 overflow-hidden p-[1px]" disabled={disabled}>
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#F9F7F7_0%,#3F72AF_50%,#F9F7F7_100%)]"></span>
                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center bg-background py-2 px-4 text-base font-medium backdrop-blur-3xl hover:text-[#ABB2BF]">
                     {children}
